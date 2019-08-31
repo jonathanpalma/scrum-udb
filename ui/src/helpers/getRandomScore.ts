07165dd1f19a25231fe593ebd45ac6c2f63cab79
Jonathan Palma <tanpalma04@gmail.com>
2019-08-31 00:10:53 -0600

    Created function to generate random score using weights

---
+++ ui/src/helpers/getRandomScore.ts
@@ -0,0 +1,24 @@
+const rand = function(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+};
+
+const getRandomScore = function() {
+  const scoreList = [1,2,3,4,5];
+  const weight = [0.01, 0.01, 0.04, 0.04, 0.9]
+  
+  const totalWeight = weight.reduce((prev, current) => prev + current);
+  const randomNum = rand(0, totalWeight);
+  let weightSum = 0;
+
+  for (let i = 0; i < scoreList.length; i++) {
+    weightSum += weight[i];
+    weightSum = +weightSum.toFixed(2);
+
+    if (randomNum <= weightSum) {
+      return scoreList[i];
+    }
+  }
+
+};
+
+export default getRandomScore;
