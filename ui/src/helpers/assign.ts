1633c769a36543b78d97d18a2f86d66617be1dd0
Hugo Chinchilla <hugo@wowfi.com>
2019-08-31 10:40:51 -0600

    Add format to scripts Added getAvailableSlots function Added getWorkLogsByDayOperator function

--- ui/src/helpers/assign.ts
+++ ui/src/helpers/assign.ts
@@ -1,23 +1,18 @@
-/*- function assign(day, job, operator)
-  - params:
-    - day: moment.Moment
-    - job: Job
-    - operator: Operator
-  - returns:
-    - workLog: WorkLog*/
-
 import moment from 'moment';
 import Job from 'interfaces/Job';
 import Operator from 'interfaces/Operator';
 import WorkLog from 'interfaces/WorkLog';
 const uuidv4 = require('uuid/v4');
 
-
 const assign = (day: moment.Moment, job: Job, operator: Operator): WorkLog => {
+  var workLog: WorkLog = {
+    id: uuidv4(),
+    idJob: job.id,
+    idOperator: operator.id,
+    date: day,
+  };
 
-    var workLog: WorkLog = { id: uuidv4(), idJob: job.id, idOperator: operator.id, date: day};
-
-    return workLog;
-}
+  return workLog;
+};
 
 export default assign;
--- ui/src/helpers/getRandomEvaluator.ts
+++ ui/src/helpers/getRandomEvaluator.ts
@@ -1,11 +1,9 @@
-import Evaluator from "interfaces/Evaluator";
+import Evaluator from 'interfaces/Evaluator';
 const uuidv4 = require('uuid/v4');
 
-
-
 const getRandomEvaluator = (): Evaluator => {
-    var evaluator: Evaluator = {id: uuidv4(), name: uuidv4()}
-    return evaluator;
-}
+  var evaluator: Evaluator = { id: uuidv4(), name: uuidv4() };
+  return evaluator;
+};
 
 export default getRandomEvaluator;
